951913610587db5ce341b15c71b90e9a124d89c6
Samuel King <samueljamesking94@gmail.com>
2024-12-14 13:19:01 +0000

    file upload for basic qwworkout app

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+import { upperBodyExercises, lowerBodyExercises, fullBodyExercises } from "./exerciseArrays.js";
+
+const mainContainer = document.getElementById("main-container");
+const button1 = document.getElementById("button-1");
+const button2 = document.getElementById("button-2");
+const button3 = document.getElementById("button-3");
+
+button1.addEventListener("click", () => {
+    renderExercises(upperBodyExercises);
+});
+
+button2.addEventListener("click", () => {
+    renderExercises(lowerBodyExercises);
+});
+
+button3.addEventListener("click", () => {
+    renderExercises(fullBodyExercises);
+});
+
+function renderExercises(exercises) {
+    mainContainer.innerHTML = "";
+
+    const shuffledExercises = [...exercises];
+
+    for (let i = shuffledExercises.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledExercises[i], shuffledExercises[j]] = [shuffledExercises[j], shuffledExercises[i]];
+    }
+
+    const selectedExercises = shuffledExercises.slice(0, 5);
+
+    selectedExercises.forEach((exercise) => {
+        const exerciseDiv = document.createElement("div");
+
+        exerciseDiv.classList.add ("card", "mb-5", "mx-auto");
+        mainContainer.appendChild(exerciseDiv);
+        exerciseDiv.innerHTML = `
+        <div class="card w-100 p-2 text-bg-dark">
+            <div class="embed-responsive">
+                <iframe class="embed-responsive-item w-100" src="${exercise.link}" allowfullscreen></iframe>
+            </div>
+
+            <div class="card-body text-center">
+                <h5 class="card-title">${exercise.name}</h5>
+                <p class="card-text">Sets: 3 Reps: 10</p>
+            </div>
+        </div>`
+
+    });
+}
---
+++ scripts/exerciseArrays.js
@@ -0,0 +1,189 @@
+export { upperBodyExercises, lowerBodyExercises, fullBodyExercises };
+
+const upperBodyExercises = [
+    {
+      name: "Push-ups",
+      link: "https://www.youtube.com/embed/_l3ySVKYVJ8?si=xIs-huZ78TLwQu1m",
+    },
+    {
+      name: "Assisted Pull-ups",
+      link: "https://youtube.com/embed/BOqrRMjxkj8?si=B1sdpMGMDBPNcqks",
+    },
+    {
+      name: "Bench Press",
+      link: "https://youtube.com/embed/SCVCLChPQFY?si=Xa64D7Xb3XQycPE0",
+    },
+    {
+      name: "Seated Overhead Press",
+      link: "https://youtube.com/embed/HzIiNhHhhtA?si=dGT8LX-aRv-YiQUD",
+    },
+    {
+      name: "Assisted Tricep Dips",
+      link: "https://youtube.com/embed/j-HCDCJKbQM?si=mW4vq5NWfFLTDxwJ",
+    },
+    {
+      name: "Dumbbell Rows",
+      link: "https://youtube.com/embed/ZRSGpBUVcNw?si=H0lJwXFjixF4VXmp",
+    },
+    {
+      name: "Bicep Curls",
+      link: "https://youtube.com/embed/SB41wiGbkaw?si=KwKeZj6niR-hvnrF",
+    },
+    {
+      name: "Dumbbell Triceps Extensions",
+      link: "https://youtube.com/embed/a9oPnZReIRE?si=PYOiasQE2MT46JrJ",
+    },
+    {
+      name: "Lateral Raises",
+      link: "https://youtube.com/embed/4q-TZNT3eHU?si=ysnma1pWjHrSj8oN",
+    },
+    {
+      name: "Front Raises",
+      link: "https://youtube.com/embed/zkP0MsTcIVU?si=hQ-jk9YfAUirYJQW",
+    },
+    {
+      name: "Shoulder Shrugs",
+      link: "https://youtube.com/embed/SX1FE0URdv8?si=OjPB7LXuLN8OJELp",
+    },
+    {
+      name: "Incline Push-ups",
+      link: "https://youtube.com/embed/yAbg3_pJKvw?si=5khwEZ463yY-xdgY",
+    },
+    {
+      name: "Diamond Push-ups",
+      link: "https://youtube.com/embed/K8bKxVcwjrk?si=h3g030_AuuOB7i1M",
+    },
+    {
+      name: "Incline Dumbbell Press",
+      link: "https://youtube.com/embed/6tW4LUaOxlE?si=CC9rWTmQSZUzHd8m",
+    },
+    {
+      name: "Decline Dumbbell Press",
+      link: "https://youtube.com/embed/vTXZEwGb3xM?si=yecMg6IiPlE2olbl",
+    },
+  ];
+  
+  const lowerBodyExercises = [
+    {
+      name: "Barbell Squats",
+      link: "https://youtube.com/embed/rrJIyZGlK8c?si=FSSACMI9Refw4QRJ",
+    },
+    {
+      name: "Dumbbell Lunges",
+      link: "https://youtube.com/embed/9gglI77Kzq8?si=gI9L0nmwW7wn6TVA",
+    },
+    {
+      name: "Deadlifts",
+      link: "https://youtube.com/embed/1ZXobu7JvvE?si=jI5vSxyrLiucPuuQ",
+    },
+    {
+      name: "Calf Raises",
+      link: "https://youtube.com/embed/ADIDoYt_ko4?si=z8ORueJeIFzzfLd1",
+    },
+    {
+      name: "Hamstring Curls",
+      link: "https://youtube.com/embed/jxctD6fL_FQ?si=VvJqbXJFdFIhjJ8_",
+    },
+    {
+      name: "Glute Bridges",
+      link: "https://youtube.com/embed/Xp33YgPZgns?si=M3GwPH0bTB1pmloM",
+    },
+    {
+      name: "Leg Press",
+      link: "https://youtube.com/embed/fM2WvgirlLM?si=kgF0fLIY0ZvtPp1q",
+    },
+    {
+      name: "Leg Extensions",
+      link: "https://youtube.com/embed/E4YZh_nZOAM?si=71eDdnOOCj-hPBdX",
+    },
+    {
+      name: "Hip Thrusts",
+      link: "https://youtube.com/embed/aweBS7K71l8?si=b8_ECxXpyfwQVK50",
+    },
+    {
+      name: "Box Jumps",
+      link: "https://youtube.com/embed/52r_Ul5k03g?si=OZyamAHE5C40EtNt",
+    },
+    {
+      name: "Bulgarian Split Squats",
+      link: "https://youtube.com/embed/vgn7bSXkgkA?si=s0GOPpTBwj2llU-i",
+    },
+    {
+      name: "Good Mornings",
+      link: "https://youtube.com/embed/nWyx81AfTos?si=3ixu0B4GVh2MTmve",
+    },
+    {
+      name: "Wall Sit",
+      link: "https://youtube.com/embed/cWTZ8Am1Ee0?si=Wq54gn_958ylnTeW",
+    },
+    {
+      name: "Single Leg Deadlifts",
+      link: "https://youtube.com/embed/84hrdsHgDuQ?si=MLY0q-TBiXi2rkoF",
+    },
+    {
+      name: "Reverse Lunges",
+      link: "https://youtube.com/embed/xrPteyQLGAo?si=9mBUcK1dLidfCPzu",
+    },
+  ];
+  
+  const fullBodyExercises = [
+    {
+      name: "Burpees",
+      link: "https://youtube.com/embed/TU8QYVW0gDU?si=LOhFU36lICdoZW0b",
+    },
+    {
+      name: "Thrusters",
+      link: "https://youtube.com/embed/L219ltL15zk?si=oO-VflVXV19XbC7J",
+    },
+    {
+      name: "Man Makers",
+      link: "https://youtube.com/embed/2-6EPLtSLwU?si=L7wA-c41J59UYpwS",
+    },
+    {
+      name: "Renegade Rows",
+      link: "https://youtube.com/embed/Q28cLuweLv4?si=Wey3A-bFLkD1fdrG",
+    },
+    {
+      name: "Kettlebell Swings",
+      link: "https://youtube.com/embed/sSESeQAir2M?si=_nT6o1PZ3uyuc58d",
+    },
+    {
+      name: "Clean and Press",
+      link: "https://youtube.com/embed/KCe8l86-alA?si=hAz_t8-w2Q-l2_NL",
+    },
+    {
+      name: "Mountain Climbers",
+      link: "https://youtube.com/embed/zT-9L3CEcmk?si=AZ-EL2nyHFKCapU5",
+    },
+    {
+      name: "Jumping Jacks",
+      link: "https://youtube.com/embed/nGaXj3kkmrU?si=fujazDHKM6CTDjLq",
+    },
+    {
+      name: "Bear Crawls",
+      link: "https://youtube.com/embed/4d52Y9W2M0g?si=UCEcXxUMKiYT7Qo1",
+    },
+    {
+      name: "Overhead Lunges",
+      link: "https://youtube.com/embed/c-4dfhO8QTw?si=2f3B2lP74YFDJ1Ga",
+    },
+    {
+      name: "Jump Squats",
+      link: "https://youtube.com/embed/BRfxI2Es2lE?si=OQ9I7qA7MwxculK_",
+    },
+    {
+      name: "Battle Ropes",
+      link: "https://youtube.com/embed/zw0OMi00X5g?si=ETNUUc_xOg1rP0En",
+    },
+    {
+      name: "Turkish Get-ups",
+      link: "https://youtube.com/embed/sgd8n917Zv0?si=AXI_wzhi4PhlOzY-",
+    },
+    {
+      name: "Box Squats",
+      link: "https://youtube.com/embed/ktMbRDQ_7fc?si=9kyODKLtBC0jUSTX",
+    },
+  ];
+  
+  
+  
